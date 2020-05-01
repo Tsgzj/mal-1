189c82bf0d7e0d951b1f21b94d374a7e33ee3f43
Tsgzj <swx@tsgzj.me>
2020-05-01 00:09:19 -0700

    Inital commit for step0. Uberjar need some debugging

---
+++ tsgzj/src/mal/step0_repl.clj
@@ -0,0 +1,25 @@
+(ns mal.step0_repl
+  (:gen-class))
+
+(defn READ [str] str)
+
+(defn EVAL [exp] exp)
+
+(defn PRINT [res] res)
+
+(defn rep [str]
+   (-> str
+       READ
+       EVAL
+       PRINT))
+
+(defn repl []
+    (do
+      (print "user> ")
+      (let [input (read-line)]
+        (when (not (= "" input))
+          (println (rep input))
+          (recur)))))
+
+(defn -main [& args]
+  (repl))
